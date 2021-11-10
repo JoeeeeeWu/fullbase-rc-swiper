8003c28c74ad06cef7c4c914426a9861a64d3e94
Joe Wu <wubaixing@heytea.com>
2021-11-10 15:48:50 +0800

    :tada: init

---
+++ babel.config.js
@@ -0,0 +1,44 @@
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = (api) => {
+  api.cache(true);
+
+  const presets = [
+    "@babel/react",
+    "@babel/env",
+    "@babel/typescript",
+  ];
+  const plugins = [
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        "corejs": {
+          "version": 3,
+          "proposals": true
+        },
+        "useEsModules": true
+      }
+    ],
+    [
+      "@babel/proposal-decorators",
+      {
+        "legacy": true
+      }
+    ],
+    [
+      "@babel/proposal-class-properties"
+    ],
+    // [
+    //   "lodash"
+    // ],
+    // isDevelopment && [
+    //   "react-refresh/babel"
+    // ],
+  ].filter(Boolean);
+
+  return {
+    presets,
+    plugins,
+    // sourceType: "unambiguous",
+  };
+};
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <>
+    <div>React Web App</div>
+  </>
+);
+
+export default App;
